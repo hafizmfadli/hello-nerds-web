5cb3f24368f1f0a8456ec1f90327c8bd8fb6625f
hafizmfadli <lshafizmf1st@gmail.com>
2022-05-03 14:45:23 +0700

    add data dummy

---
+++ src/data.js
@@ -0,0 +1,61 @@
+export const sliderItems = [
+  {
+    id: 1,
+    bg: "f5fafd",
+    title: "Summer Sale",
+    desc: "BEST SELLER SELF DEVELOPMENT BOOK! GET DISCOUNT 30% OFF",
+    img: "https://cf.shopee.co.id/file/749f58aea3e0754929823bd113bdc73b"
+  },
+  {
+    id: 2,
+    bg: "f5fafd",
+    title: "Winter Sale",
+    desc: "BEST SELLER BUSINESS BOOK! GET DISCOUNT 30% OFF",
+    img: "https://images-na.ssl-images-amazon.com/images/I/5112YFsXIJL.jpg"
+  },
+  {
+    id: 3,
+    bg: "f5fafd",
+    title: "Computer Science Day",
+    desc: "BEST SELLER Javascript BOOK! GET DISCOUNT 30% OFF",
+    img: "https://miro.medium.com/max/1200/1*zBRkcBbsjWzcPjtcxqhb3Q.jpeg"
+  }
+]
+
+export const carouselProducts = [
+  {
+    id: 1,
+    imgUrl: '/assets/no-image.jpeg',
+    title: 'How to make money just in the bedroom',
+    author: 'Hafiz, Fadli',
+    price: 50000
+  },
+  {
+    id: 2,
+    imgUrl: '/assets/no-image.jpeg',
+    title: 'Elasticsearch definitive guide',
+    author: 'Hafiz, Fadli',
+    price: 150000
+  },
+  {
+    id: 3,
+    imgUrl: '/assets/no-image.jpeg',
+    title: 'Mongo, Express, React, and Node (MERN) Stack',
+    author: 'Hafiz, Fadli',
+    price: 150000
+  },
+  {
+    id: 4,
+    imgUrl: '/assets/no-image.jpeg',
+    title: 'Apache Kafka and Debezium',
+    author: 'Hafiz, Fadli',
+    price: 150000
+  },
+  {
+    id: 5,
+    imgUrl: '/assets/no-image.jpeg',
+    title: 'How Linux Works',
+    author: 'Hafiz, Fadli',
+    price: 150000
+  }
+]
