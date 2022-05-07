7ba77ea1ee2c38220636a8cee8f68c885d1cf5ef
hafizmfadli <lshafizmf1st@gmail.com>
2022-05-07 22:35:15 +0700

    update routing rule

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import { Container } from "@mui/material";
-import styled from "styled-components";
-import { textAlign } from "@mui/system";
-
-const ProductLink = styled.a`
-  text-decoration: none;
-  color: inherit;
-`;
+import { Link } from "react-router-dom";
 
 const ProductItem = ({
   productId,
@@ -15,6 +9,7 @@ const ProductItem = ({
   price,
   author,
 }) => {
+
   return (
     <Container sx={{
       width: '200px',
@@ -25,15 +20,14 @@ const ProductItem = ({
       textAlign: 'center',
       border: '1px solid #b9b9b9'
     }}>
-      <ProductLink
-        href={`/product/${productId}`}
-        onClick={() => alert(`${productTitle} clicked`)}
+      <Link
+        to={`/p/${productId}/${productTitle}`}
       >
         <img src="/assets/no-image.jpeg" alt="product-vocer" width="120" height="120" />
         <p>{productTitle}</p>
         <p>Author: {author === "" ? "?" : author}</p>
         <p>Rp {price}</p>
-      </ProductLink>
+      </Link>
     </Container>
   );
 };
